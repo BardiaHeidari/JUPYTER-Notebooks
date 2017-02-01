f316e906a38a9242b8ce9f894876d0e47afc844e
leonard-psu <leonard-psu@users.noreply.github.com>
2017-02-01 09:39:12 -0500

    Update GI_Soil.py
    Add user defined soil shapefile name

--- GI_Soil.py
+++ GI_Soil.py
@@ -14,6 +14,7 @@ from ecohydrolib.ssurgo.featurequery import SSURGO_BBOX_TILE_DIVISOR
 from ecohydrolib.ssurgo import featurequery
 
 parser = argparse.ArgumentParser(description='Update MetaData file to use GI Soils')
+parser.add_argument('-s', '--shapefile', dest='shpFilename', required=True, help='The name of the GI soil shapefile')
 parser.add_argument('-p', '--projectDir', dest='projectDir', required=True, help='The directory to which metadata, intermediate, and final files should be saved')
 parser.add_argument('--overwrite', dest='overwrite', action='store_true', required=False,help='Overwrite existing SSURGO features shapefile in project directory.  If not specified, program will halt if a dataset already exists.')
 
@@ -34,7 +35,7 @@ if 'soil_features' in manifest:
         sys.exit( textwrap.fill('SSURGO features already exist in project directory.  Use --overwrite option to overwrite.') )
 
 
-shpFilename = 'MapunitPolyExtended.shp'
+shpFilename = args.shpFilename ###'MapunitPolyExtended.shp'
 
 # Write provenance
 asset = AssetProvenance(GenericMetadata.MANIFEST_SECTION)
